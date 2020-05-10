617fdb745b900836e6d8e9b920128fb43cb62d72
Jerrylum <serverofjerry@gmail.com>
2020-05-10 18:15:30 +0800

    Add stopwatch add button disabled tooltips

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ class TimerStatus {
     static TIMESUP = Symbol();
 }
 
-class CountdownTimer {
+class Timer {
     constructor() {
         this._previousTicks = 0;
-        this._totalTicks = 0;
         this._startTick = null;
     }
 
@@ -22,22 +21,29 @@ class CountdownTimer {
     }
 
     start() {
-        if (!this.isRunning && !this.isTimeup)
-            this._startTick = new Date().getTime();
+        throw new Error("NotImplementedError");
+    }
+
+    /**
+     * Set the time value of this timer
+     * @param {number} a Hour or tick
+     * @param {number} b Minute or undefined
+     * @param {number} c Second
+     */
+    set(a, b, c) {
+        throw new Error("NotImplementedError");
     }
 
     reset() {
-        this._previousTicks = 0;
         this._startTick = null;
-    }
-
-    set(h, m, s) {
         this._previousTicks = 0;
-        this._totalTicks = h * 3600000 + m * 60000 + s * 1000;
+
+        let { hour, minute, second } = timerEditableData;
+        this.set(hour, minute, second);
     }
 
-    get isTimeup() {
-        return this.status == TimerStatus.TIMESUP;
+    get displayTicks() {
+        throw new Error("NotImplementedError");
     }
 
     get isRunning() {
@@ -45,6 +51,93 @@ class CountdownTimer {
     }
 
     get status() {
+        throw new Error("NotImplementedError");
+    }
+}
+
+class StopwatchTimer extends Timer {
+    constructor() {
+        super();
+
+        this._initTicks = 0;
+    }
+
+    start() { // override
+        if (!this.isRunning)
+            this._startTick = new Date().getTime();
+    }
+
+    set(a, b, c) {
+        if (b != null && c != null)
+            a = a * 3600000 + b * 60000 + c * 1000;
+
+        if (this.status == TimerStatus.PAUSE) {
+            this._initTicks = a + this._previousTicks % 1000 + 1; // keep ms
+            this._previousTicks = 1; // keep pause mode
+        } else {
+            this._initTicks = a;
+        }
+    }
+
+    get displayTicks() {
+        return this.passedTicks;
+    }
+
+    get status() { // override
+        if (this._startTick == null && this._previousTicks == 0)
+            return TimerStatus.INIT;
+        else if (this._startTick == null && this._previousTicks != 0)
+            return TimerStatus.PAUSE;
+        else
+            return TimerStatus.RUNNING;
+    }
+
+    get passedTicks() {
+        return Math.max(
+            0,
+            (
+                this._startTick ?
+                    new Date().getTime() - this._startTick :
+                    0
+            ) +
+            this._initTicks + this._previousTicks
+        );
+    }
+}
+
+class CountdownTimer extends Timer {
+    constructor() {
+        super();
+
+        this._totalTicks = 0;
+    }
+
+    start() { // override
+        if (!this.isRunning && !this.isTimeup)
+            this._startTick = new Date().getTime();
+    }
+
+    set(a, b, c) {
+        if (b != null && c != null)
+            a = a * 3600000 + b * 60000 + c * 1000;
+
+        if (this.status == TimerStatus.PAUSE) {
+            this._totalTicks = a + this._previousTicks % 1000 + 1; // keep ms
+            this._previousTicks = 1; // keep pause mode
+        } else {
+            this._totalTicks = a;
+        }
+    }
+
+    get displayTicks() {
+        return this.remainingTicks;
+    }
+
+    get isTimeup() {
+        return this.status == TimerStatus.TIMESUP;
+    }
+
+    get status() { // override
         if (this._startTick == null && this._previousTicks == 0)
             return TimerStatus.INIT;
         else if (this._startTick != null && this.remainingTicks != 0)
@@ -71,11 +164,12 @@ class CountdownTimer {
             this._totalTicks - this._previousTicks
         );
     }
+
 }
 
-function setFieldContent(elm, time, zero = 2) {
+function setFieldContent(elm, time, col = 2) {
     time = Math.trunc(time);
-    elm.innerText = time > 9 ? time + '' : (zero == 3 ? '00' : '0') + time;
+    elm.innerText = time > 9 ? time + '' : (col == 3 ? '00' : '0') + time;
 }
 
 function getAllTimeValueField() {
@@ -84,6 +178,22 @@ function getAllTimeValueField() {
         document.querySelector('#timer-value #M'),
         document.querySelector('#timer-value #S')
     ];
+}
+
+function updateEditableDataToTimer(target) {
+    let value = Math.trunc(target.innerText);
+
+    let target_id = target.id;
+    if (target_id === 'H') {
+        timerEditableData.hour = value;
+    } else if (target_id === 'M') {
+        timerEditableData.minute = value;
+    } else if (target_id === 'S') {
+        timerEditableData.second = value;
+    }
+
+    let { hour, minute, second } = timerEditableData;
+    t.set(hour, minute, second);
 }
 
 function focusTimeValueFieldEvent(e) {
@@ -98,26 +208,14 @@ function blurTimeValueFieldEvent(e) {
     let value_str = target.innerText;
     let value = Math.trunc(value_str);
 
+    // important
     if (value_str.length == 0 || value_str.length > 2 || isNaN(value) || value_str.indexOf('.') !== -1) {
         target.innerText = target.beforeFocus;
     } else {
         value = Math.max(0, Math.min(value, 59));
         setFieldContent(target, value);
-
-
-        let target_id = target.id;
-        if (target_id === 'H') {
-            timerEditableData.hour = value;
-        } else if (target_id === 'M') {
-            timerEditableData.minute = value;
-        } else if (target_id === 'S') {
-            timerEditableData.second = value;
-        }
-
-        let { hour, minute, second } = timerEditableData;
-        t.set(hour, minute, second);
     }
-
+    updateEditableDataToTimer(target);
 }
 
 function b4changedTimeValueFieldEvent(e) {
@@ -149,6 +247,9 @@ function changedTimeValueFieldEvent(e) {
     newRange.collapse(true);
     sel.removeAllRanges();
     sel.addRange(newRange);
+
+    // finally, update the data
+    updateEditableDataToTimer(target);
 }
 
 function nextTimeValueFieldEvent(e) {
@@ -165,9 +266,27 @@ function nextTimeValueFieldEvent(e) {
     }
 }
 
+function clickBtn0Event() {
+    if (t.status == TimerStatus.INIT) {
+        let oldClockTicks = t.displayTicks;
+        t = t instanceof CountdownTimer ? new StopwatchTimer() : new CountdownTimer();
+        t.set(oldClockTicks);
+
+        let btn0 = document.querySelector('#btn0 .content');
+        if (t instanceof CountdownTimer) {
+            btn0.innerText = 'Timer';
+        } else {
+            btn0.innerText = 'Stopwatch';
+        }
+    }
+}
+
 function clickBtn1Event() {
-    if (t.status == TimerStatus.INIT && t.totalTicks != 0)
-        t.start();
+    if (t.status == TimerStatus.INIT)
+        if ((t instanceof CountdownTimer) && (t.totalTicks == 0))
+            return;
+        else
+            t.start();
     else if (t.status == TimerStatus.RUNNING)
         t.stop();
     else if (t.status == TimerStatus.PAUSE)
@@ -178,7 +297,22 @@ function clickBtn2Event() {
     t.reset();
 }
 
+function enableButton(elm, text) {
+    elm.className = '';
+    if (text)
+        elm.querySelector('.content').innerText = text;
+}
+
+function disableButton(elm, text, msg) {
+    elm.className = 'disabled';
+    if (text)
+        elm.querySelector('.content').innerText = text;
+    if (msg)
+        elm.querySelector('.tooltiptext').innerText = msg;
+}
+
 function updateTimerEvent() {
+    let btn0 = document.getElementById('btn0');
     let btn1 = document.getElementById('btn1');
     let fields = getAllTimeValueField();
     let msField = document.getElementById('timer-value-msec');
@@ -187,11 +321,11 @@ function updateTimerEvent() {
     let nowStatus = t.status;
 
     if (nowStatus == TimerStatus.TIMESUP) {
-        if (this.lastStatus != nowStatus) {
+        if (updateTimerEvent.lastStatus != nowStatus) {
             fields.forEach((x) => x.setAttribute('contenteditable', 'false'));
 
-            btn1.className = 'disabled';
-            btn1.innerText = 'Start';
+            disableButton(btn0);
+            disableButton(btn1, 'Start', 'Time is up, please reset the timer first');
 
             setFieldContent(fields[0], 0);
             setFieldContent(fields[1], 0);
@@ -200,16 +334,16 @@ function updateTimerEvent() {
         }
 
     } else if (nowStatus == TimerStatus.RUNNING) {
-        if (this.lastStatus != nowStatus) {
+        if (updateTimerEvent.lastStatus != nowStatus) {
 
             fields.forEach((x) => x.setAttribute('contenteditable', 'false'));
 
-            btn1.className = '';
-            btn1.innerText = 'Pause';
+            disableButton(btn0);
+            enableButton(btn1, 'Pause');
         }
 
 
-        let ticks = t.remainingTicks;
+        let ticks = t.displayTicks;
 
         setFieldContent(fields[0], ticks / 3600000);
         ticks %= 3600000;
@@ -220,17 +354,17 @@ function updateTimerEvent() {
         setFieldContent(msField, ticks, 3);
 
     } else if (nowStatus == TimerStatus.PAUSE) {
-        if (this.lastStatus != nowStatus) {
-             // the user can edit the time
+        if (updateTimerEvent.lastStatus != nowStatus) {
+            // the user can edit the time
             fields.forEach((x) => x.setAttribute('contenteditable', 'true'));
 
-            btn1.className = '';
-            btn1.innerText = 'Start';
+            disableButton(btn0);
+            enableButton(btn1, 'Start');
         }
 
     } else { // init status, make the field editable
 
-        if (this.lastStatus != nowStatus) {
+        if (updateTimerEvent.lastStatus != nowStatus) {
             let { hour, minute, second } = timerEditableData;
 
             setFieldContent(fields[0], hour);
@@ -240,13 +374,17 @@ function updateTimerEvent() {
 
             fields.forEach((x) => x.setAttribute('contenteditable', 'true'));
 
-            btn1.className = '';
-            btn1.innerText = 'Start';
+            enableButton(btn0);
+
         }
 
+        if (t instanceof CountdownTimer && t.displayTicks == 0)
+            disableButton(btn1, 'Start', 'Timer cannot be set to zero');
+        else
+            enableButton(btn1, 'Start');
     }
 
-    this.lastStatus = nowStatus;
+    updateTimerEvent.lastStatus = nowStatus;
 }
 
 
