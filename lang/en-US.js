c9b4fe0f1689fbf6c029d46f5111eba1799230dd
Jerry Lum <serverofjerry@gmail.com>
2020-10-27 13:12:42 +0800

    change code structure

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,1 +1,19 @@
-"use strict";useLanguage({name:"en-US",start:"Start",pause:"Pause",reset:"Reset",clear:"Clear",times_up_blocked_warning:"Time is up, please reset the timer first",change_language:"Change language",change_theme:"Change theme",get_help:"Get help",get_help_link:"./intro/en",hide_ui:"Hide buttons",theme:{dark:"Dark",light:"Light"}});
+"use strict";
+
+useLanguage({
+  name: 'en-US',
+  start: 'Start',
+  pause: 'Pause',
+  reset: 'Reset',
+  clear: 'Clear',
+  times_up_blocked_warning: 'Time is up, please reset the timer first',
+  change_language: 'Change language',
+  change_theme: 'Change theme',
+  get_help: 'Get help',
+  get_help_link: './intro/en',
+  hide_ui: 'Hide buttons',
+  theme: {
+    dark: 'Dark',
+    light: 'Light'
+  }
+});
--- lang/zh-HK.js
+++ lang/zh-HK.js
@@ -1,1 +1,19 @@
-"use strict";useLanguage({name:"zh-HK",start:"開始",pause:"暫停",reset:"重設",clear:"清除",times_up_blocked_warning:"時間到，請先重設計時器",change_language:"更改顯示語言",change_theme:"更改顏色主題",get_help:"取得幫助",get_help_link:"./intro/",hide_ui:"隱藏介面按鈕",theme:{dark:"暗黑",light:"光亮"}});
+"use strict";
+
+useLanguage({
+  name: 'zh-HK',
+  start: '開始',
+  pause: '暫停',
+  reset: '重設',
+  clear: '清除',
+  times_up_blocked_warning: '時間到，請先重設計時器',
+  change_language: '更改顯示語言',
+  change_theme: '更改顏色主題',
+  get_help: '取得幫助',
+  get_help_link: './intro/',
+  hide_ui: '隱藏介面按鈕',
+  theme: {
+    dark: '暗黑',
+    light: '光亮'
+  }
+});
